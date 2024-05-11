ca9b49cd2abc373fad347ed62d0a03bb990c1133
allaboutraj <rajgupta9507321@gmail.com>
2024-05-11 21:00:05 +0530

    Added: BullBoard an Queue background jobs inspector

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import bodyParser from 'body-parser';
 import express, {Express} from 'express';
 
+import bullBoardAdapter from './config/bullBoardConfig';
 import serverConfig from './config/serverConfig';
 import sampleQueueProducer from './producers/sampleQueueProducer';
 import apiRouter from './routes';
@@ -7,10 +9,18 @@ import SampleWorker from './workers/SampleWorker';
 
 const app: Express = express();
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+
 app.use('/api', apiRouter);
+app.use('/ui', bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
     console.log(`listening on port :${serverConfig.PORT}`);
+    console.log(`BullBoard dashboard running on: 
+    http://localhost:${serverConfig.PORT}/ui`);
 
     SampleWorker('SampleQueue');
 
