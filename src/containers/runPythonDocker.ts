57c05fe10519a7459885929bde56393c5d529db0
allaboutraj <rajgupta9507321@gmail.com>
2024-06-04 00:13:49 +0530

    Added removing container

--- src/containers/runPythonDocker.ts
+++ src/containers/runPythonDocker.ts
@@ -38,16 +38,22 @@ async function runPython(code: string, inputTestCase: string) {
         rawLogBuffer.push(chunk);
     });
 
-    loggerStream.on('end', () => {
-        console.log(rawLogBuffer);
-        const completeBuffer = Buffer.concat(rawLogBuffer);
-        const decodedStream = decodeDockerStream(completeBuffer);
-        console.log(decodedStream);
-        console.log(decodedStream.stdout);
-
+    await new Promise((res) => {
+        loggerStream.on('end', () => {
+            console.log(rawLogBuffer);
+            const completeBuffer = Buffer.concat(rawLogBuffer);
+            const decodedStream = decodeDockerStream(completeBuffer);
+            console.log(decodedStream);
+            console.log(decodedStream.stdout);
+            res(decodeDockerStream);
+    
+        });
     });
 
-    return pythonDockerContainer;
+    await pythonDockerContainer.remove();
+    
+
+    // return pythonDockerContainer;
 }       
 
 export default runPython;
